23c77e4401dadec59747d5f96ffc5ab9f19526b3
diegosouzacosta <diego.costa@shiftit.com.br>
2013-03-04 16:36:12 -0300

    Adicionado email no admin e página inicial - landing page de construcao

--- PyProject_ShiftIT/urls.py
+++ PyProject_ShiftIT/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults      import patterns, include, url
 from django.contrib                 import admin
 
+from django.conf                    import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
+    (r'^media/(.*)$', 'django.views.static.serve',
+     {'document_root': settings.MEDIA_ROOT}),
     (r'^', include('flatpages.urls')),
     (r'^', include('comunicacao.urls')),
     (r'^', include('autenticacao.urls')),
